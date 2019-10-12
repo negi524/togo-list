9dd6d965866f348271b684be3000180f775726b6
negi524 <egtnrumpbiizarre.info@gmail.com>
2019-10-13 01:03:26 +0900

    リストの見た目調整

--- pages/list.vue
+++ pages/list.vue
@@ -1,15 +1,19 @@
 <template>
-  <div id="list">
-    <h1>list</h1>
-    <ul>
-      <li v-for="togo in togos">
-        <!-- <input type="checkbox" :checked="togos.done" @change="toggle(togo)" /> -->
-        <span>{{ togo.pid }}</span>
-        <span>{{ togo.about }}</span>
-        <span>{{ togo.created }}</span>
-      </li>
-    </ul>
+  <div class="list">
     <!-- <input placeholder="リストを追加" @keyup.enter="addTogo" /> -->
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>行きたいところリスト</span>
+        <!-- <el-button type="text">Operation button</el-button> -->
+      </div>
+      <div v-for="togo in togos" :key="togo.pid" class="text item">{{ togo.about }}</div>
+    </el-card>
+    <!-- <el-calendar v-model="value"></el-calendar> -->
+    <el-table :data="togos">
+      <el-table-column prop="pid" label="Pid" width="180"></el-table-column>
+      <el-table-column prop="about" label="About" width="280"></el-table-column>
+      <el-table-column prop="created" label="Created"></el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -21,6 +25,9 @@ export default {
     this.$store.dispatch("togos/fetchTogo");
   },
   computed: {
+    value: function() {
+      return new Date();
+    },
     togos: function() {
       return this.$store.state.togos.list;
     }
@@ -37,7 +44,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 h1 {
   margin-bottom: 20px;
 }
