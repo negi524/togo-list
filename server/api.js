37423bef33962318c2ac28e54cf4a4bd7a8c742a
negi524 <egtnrumpbiizarre.info@gmail.com>
2019-10-12 15:38:01 +0900

    APIのエンドポイント作成

---
+++ server/api.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/hoge", (req, res, next) => {
+  const param = { test: "success" };
+  res.header("Content-Type", "application/json");
+  res.send(param);
+});
+
+module.exports = router;
