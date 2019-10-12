664f809f8fd321d8ca8a51ca1e535453ea4e53d4
negi524 <egtnrumpbiizarre.info@gmail.com>
2019-10-12 19:03:49 +0900

    use knex, bookshelf

--- server/api.js
+++ server/api.js
@@ -5,23 +5,37 @@ const router = express.Router();
 const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database("./db/mydb.sqlite3");
 
-router.get("/hoge", (req, res, next) => {
-  const param = { test: "success" };
-  res.header("Content-Type", "application/json");
-  res.send(param);
+// knexの設定
+const knex = require("knex")({
+  client: "sqlite3", // 使用するDBの種類
+  // データベースの接続情報
+  connection: {
+    filename: "./db/mydb.sqlite3"
+  },
+  useNullAsDefault: true // sqliteを利用する場合のみ、true
 });
+
+// bookshelfの設定
+const BookShelf = require("bookshelf")(knex);
+
+// usersテーブルにアクセスするためのMyDataオブジェクト
+const MyData = BookShelf.Model.extend({ tableName: "users" });
 
 /**
  * user情報を取得するエンドポイント
  */
 router.get("/users", (req, res, next) => {
-  db.serialize(() => {
-    db.all("select * from users", (err, rows) => {
-      console.dir(rows);
-      res.header("Content-Type", "application/json");
-      res.send(rows);
+  new MyData()
+    .fetchAll()
+    .then(collection => {
+      res.send(collection);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: true,
+        data: { message: err.message }
+      });
     });
-  });
 });
 
 /**
