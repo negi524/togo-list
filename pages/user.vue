92ba91e07cfaca84dc9c69ad8b26bbc8f17f256f
negi524 <egtnrumpbiizarre.info@gmail.com>
2019-10-12 18:42:18 +0900

    DBの情報を画面に表示

---
+++ pages/user.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button @click="getIp">IPアドレスを取得</button>
+    <p>{{ ip }}</p>
+    <hr />
+    <button @click="getUser">ユーザー情報取得</button>
+    <p>{{ user }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      ip: "0.0.0.0",
+      user: []
+    };
+  },
+  methods: {
+    async getIp() {
+      this.ip = await this.$axios.$get("http://icanhazip.com");
+    },
+    async getUser() {
+      this.user = await this.$axios.$get("/api/users");
+    }
+  }
+};
+</script>
