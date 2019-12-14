769f64bcd27071c07da20df8e7f78714c7ea964d
negi524 <egtnrumpbiizarre.info@gmail.com>
2019-12-15 00:55:58 +0900

    form追加

--- pages/list.vue
+++ pages/list.vue
@@ -1,41 +1,15 @@
 <template>
   <div>
-    <Table />
-    <!-- <v-form class="togo__form" ref="form">
-      <v-text-field label="名称" v-model="form.name" required></v-text-field>
-      <v-text-field
-        label="最寄駅"
-        v-model="form.station"
-        required
-      ></v-text-field>
-      <v-text-field
-        label="都道府県"
-        v-model="form.prefectures"
-        required
-      ></v-text-field>
-      <v-btn class="mr-4" @click="pushTogo">send button</v-btn>
-    </v-form> -->
+    <TogoTable />
   </div>
 </template>
 
 <script>
-import Table from "@/components/Table.vue";
+import TogoTable from "@/components/TogoTable.vue";
 
 export default {
   components: {
-    Table
-  },
-  data: function() {
-    return {
-      form: {
-        index: 0,
-        name: "",
-        station: "",
-        url: "",
-        prefectures: "",
-        created: null
-      }
-    };
+    TogoTable
   }
 };
 </script>
