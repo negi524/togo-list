35231e2adf0dd5ce08d68686ca9324198654d8c0
negi524 <egtnrumpbiizarre.info@gmail.com>
2019-12-14 16:25:59 +0900

    ヘッダーのコンポーネントを分離

---
+++ components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <header class="header">
+    <h1 class="header__title">
+      <i class="el-icon-folder-opened"></i>
+      My Application
+    </h1>
+    <nav class="header-nav">
+      <ul class="header-nav__wrapper">
+        <li class="header-nav__item">
+          <a href>about me</a>
+        </li>
+        <li class="header-nav__item">
+          <a href>to go list</a>
+        </li>
+        <li class="header-nav__item">
+          <a href="https://github.com/negi524">GitHub</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/colors/default.scss";
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+  padding: 30px 15px 30px;
+  background-color: $pistachio;
+
+  &__title {
+    font-size: 25px;
+  }
+}
+
+.header-nav {
+  margin: auto 0 0;
+  font-size: 18px;
+  &__wrapper {
+    display: flex;
+    justify-content: space-around;
+    width: 500px;
+  }
+}
+</style>
