b2254c1f8d813838dbfba5f53b675088fee511c9
negi524 <egtnrumpbiizarre.info@gmail.com>
2019-12-15 00:15:52 +0900

    リスト表示部分をコンポーネント化

--- pages/list.vue
+++ pages/list.vue
@@ -1,24 +1,31 @@
 <template>
   <div>
-    <el-table :data="togos">
-      <el-table-column prop="station" label="最寄駅" width="180"></el-table-column>
-      <el-table-column prop="name" label="名称" width="280"></el-table-column>
-      <el-table-column prop="prefectures" label="都道府県"></el-table-column>
-      <el-table-column prop="created" label="作成日時"></el-table-column>
-    </el-table>
-    <v-form class="togo__form" ref="form">
+    <Table />
+    <!-- <v-form class="togo__form" ref="form">
       <v-text-field label="名称" v-model="form.name" required></v-text-field>
-      <v-text-field label="最寄駅" v-model="form.station" required></v-text-field>
-      <v-text-field label="都道府県" v-model="form.prefectures" required></v-text-field>
+      <v-text-field
+        label="最寄駅"
+        v-model="form.station"
+        required
+      ></v-text-field>
+      <v-text-field
+        label="都道府県"
+        v-model="form.prefectures"
+        required
+      ></v-text-field>
       <v-btn class="mr-4" @click="pushTogo">send button</v-btn>
-    </v-form>
+    </v-form> -->
   </div>
 </template>
 
 <script>
 import { mapMutations } from "vuex";
+import Table from "@/components/Table.vue";
 
 export default {
+  components: {
+    Table
+  },
   data: function() {
     return {
       form: {
@@ -30,27 +37,8 @@ export default {
         created: null
       }
     };
-  },
-  async created() {
-    this.$store.dispatch("togos/fetchTogo");
-  },
-  computed: {
-    togos: function() {
-      return this.$store.state.togos.list;
-    }
-  },
-  methods: {
-    pushTogo(e) {
-      console.log(this.form);
-      this.$store.dispatch("togos/pushTogo", this.form);
-      this.form = {}; // 追加したので、フォームの内容を空にする
-    }
   }
 };
 </script>
 
-<style scoped lang="scss">
-h1 {
-  margin-bottom: 20px;
-}
-</style>
+<style scoped lang="scss"></style>
