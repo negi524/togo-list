39962a0d05875b0e281d6030e5ab0b264b17ca60
negi524 <egtnrumpbiizarre.info@gmail.com>
2019-12-15 19:41:20 +0900

    サイドバーのコンポーネント削除

--- test/GlobalSidebar.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from "@vue/test-utils";
-import GlobalSidebar from "@/components/GlobalSidebar.vue";
-
-describe("GlobalSidebar", () => {
-  test("is a Vue instance", () => {
-    const wrapper = mount(GlobalSidebar);
-    expect(wrapper.isVueInstance()).toBeTruthy();
-  });
-});
