a29516f9e3be26b04243efb0112665afe8e5496f
negi524 <egtnrumpbiizarre.info@gmail.com>
2020-01-03 12:28:45 +0900

    margin調整

--- pages/edit.vue
+++ pages/edit.vue
@@ -3,14 +3,14 @@
     <h2 class="heading">データ一覧</h2>
     <TogoTable />
     <hr />
-    <h2 class="heading">データを追加する</h2>
-    <b-form-input v-model="form.name" placeholder="名称"></b-form-input>
-    <b-form-input v-model="form.prefectures" placeholder="都道府県"></b-form-input>
-    <b-form-input v-model="form.station" placeholder="最寄駅"></b-form-input>
+    <h2 class="heading">データ追加</h2>
+    <b-form-input class="mb-2" v-model="form.name" placeholder="名称"></b-form-input>
+    <b-form-input class="mb-2" v-model="form.prefectures" placeholder="都道府県"></b-form-input>
+    <b-form-input class="mb-2" v-model="form.station" placeholder="最寄駅"></b-form-input>
     <b-button @click="pushTogo">データ登録</b-button>
     <hr />
-    <h2 class="heading">データを削除する</h2>
-    <b-form-input v-model="deleteName" placeholder="削除対象の名称"></b-form-input>
+    <h2 class="heading">データ削除</h2>
+    <b-form-input class="mb-2" v-model="deleteName" placeholder="削除対象の名称"></b-form-input>
     <b-button @click="deleteTogo">データ削除</b-button>
   </div>
 </template>
