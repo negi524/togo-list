436ee9aa934d3f2a1de6707a5c24bb1c9d619550
negi524 <egtnrumpbiizarre.info@gmail.com>
2020-01-10 06:59:25 +0900

    togoリストのカードテンプレートを作成

---
+++ components/TogoCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{ togo }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    togo: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+</style>
--- pages/list.vue
+++ pages/list.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
     <TogoTable />
+    <togo-card v-for="togo in togos" :key="togo.name" :togo="togo" />
   </div>
 </template>
 
 <script>
 import TogoTable from "@/components/TogoTable.vue";
+import TogoCard from "@/components/TogoCard.vue";
 
 export default {
   components: {
-    TogoTable
+    TogoTable,
+    TogoCard
+  },
+  async created() {
+    this.$store.dispatch("togos/fetchTogo");
+  },
+  computed: {
+    togos: function() {
+      return this.$store.state.togos.list;
+    }
   }
 };
 </script>
