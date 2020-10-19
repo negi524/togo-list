5b22279194fa10a2391b36967d246081b5019100
negi524 <egtnrumpbiizarre.info@gmail.com>
2020-10-20 00:44:19 +0900

    firebase認証を実装

--- pages/edit.vue
+++ pages/edit.vue
@@ -1,56 +1,96 @@
 <template>
   <div>
+    <h1>{{ message }}</h1>
     <h2 class="heading">データ一覧</h2>
     <TogoTable />
     <hr />
     <h2 class="heading">データ追加</h2>
-    <b-form-input class="mb-2" v-model="form.name" placeholder="名称"></b-form-input>
-    <b-form-input class="mb-2" v-model="form.prefectures" placeholder="都道府県"></b-form-input>
-    <b-form-input class="mb-2" v-model="form.station" placeholder="最寄駅"></b-form-input>
+    <b-form-input
+      class="mb-2"
+      v-model="form.name"
+      placeholder="名称"
+    ></b-form-input>
+    <b-form-input
+      class="mb-2"
+      v-model="form.prefectures"
+      placeholder="都道府県"
+    ></b-form-input>
+    <b-form-input
+      class="mb-2"
+      v-model="form.station"
+      placeholder="最寄駅"
+    ></b-form-input>
     <b-button @click="pushTogo">データ登録</b-button>
     <hr />
     <h2 class="heading">データ削除</h2>
-    <b-form-input class="mb-2" v-model="deleteName" placeholder="削除対象の名称"></b-form-input>
+    <b-form-input
+      class="mb-2"
+      v-model="deleteName"
+      placeholder="削除対象の名称"
+    ></b-form-input>
     <b-button @click="deleteTogo">データ削除</b-button>
   </div>
 </template>
 
 <script>
 import TogoTable from "@/components/TogoTable.vue";
+import firebase from "firebase";
 
 export default {
   components: {
-    TogoTable
+    TogoTable,
   },
-  data: function() {
+  data: function () {
     return {
+      message: "this is message",
       form: {
         name: "",
         station: "",
         prefectures: "",
-        created: null
+        created: null,
       },
-      deleteName: ""
+      deleteName: "",
     };
   },
+  created() {
+    const firebaseConfig = {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.FIREBASE_DB_URL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APPID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    };
+    firebase.initializeApp(firebaseConfig);
+    let provider = new firebase.auth.GoogleAuthProvider();
+    let self = this;
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function (result) {
+        self.message = result.user.displayName + ", " + result.user.email;
+      });
+  },
   methods: {
-    pushTogo: function() {
+    pushTogo: function () {
       this.$store.dispatch("togos/pushTogo", this.form);
       // フォームの値をリセットする
       this.form.name = "";
       this.form.station = "";
       this.form.prefectures = "";
     },
-    deleteTogo: function() {
+    deleteTogo: function () {
       // this.$store.dispatch("togos/deleteTogoByIndex", 0);
       const obj = {
-        name: this.deleteName
+        name: this.deleteName,
       };
       this.$store.dispatch("togos/deleteTogoByObj", obj);
       // フォームの値をリセットする
       this.deleteName = "";
-    }
-  }
+    },
+  },
 };
 </script>
 
