0694b000ba5dc1c5d65fd19ca20b71b85273a818
negi524 <egtnrumpbiizarre.info@gmail.com>
2020-10-31 02:53:56 +0900

    ヘッダーにログイン/ログアウト処理追加

--- components/GlobalHeader.vue
+++ components/GlobalHeader.vue
@@ -11,11 +11,49 @@
           <b-nav-item :to="'list'">一覧ページ</b-nav-item>
           <b-nav-item :to="'edit'">編集ページ</b-nav-item>
           <b-nav-item href="https://github.com/negi524">GitHub</b-nav-item>
+          <b-nav-item-dropdown text="User" right>
+            <b-dropdown-item @click="changeLoginStatus">{{
+              loginAction
+            }}</b-dropdown-item>
+          </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
   </header>
 </template>
+
+<script>
+export default {
+  computed: {
+    status() {
+      return this.$store.state.user.login;
+    },
+    loginAction() {
+      return this.$store.state.user.login ? "ログアウトする" : "ログインする";
+    },
+  },
+  methods: {
+    /**
+     * ログイン or ログアウトを行う
+     */
+    changeLoginStatus() {
+      this.status ? this.logout() : this.login();
+    },
+    /**
+     * ログインする
+     */
+    login() {
+      this.$store.dispatch("user/login");
+    },
+    /**
+     * ログアウトする
+     */
+    logout() {
+      this.$store.dispatch("user/logout");
+    },
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 @import "@/assets/style/colors.scss";
